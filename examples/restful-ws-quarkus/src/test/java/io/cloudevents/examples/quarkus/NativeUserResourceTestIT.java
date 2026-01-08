9172cca458f187fe3d0b07b032b59cbbdf4b42f1
Dennis Wittkötter <dw@logentis.de>
2026-01-08 09:34:50 +0100

    chore: delete examples

--- examples/restful-ws-quarkus/src/test/java/io/cloudevents/examples/quarkus/NativeUserResourceTestIT.java
+++
@@ -1,9 +0,0 @@
-package io.cloudevents.examples.quarkus;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeUserResourceTestIT extends UserResourceTest {
-
-    // Execute the same tests but in native mode.
-}
